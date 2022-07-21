1480d6f3214d53112c98a4089a58da5d834dbcff
UrielCh <uriel.chemouni@gmail.com>
2022-07-21 08:42:54 +0300

    add proxy

---
+++ proxy/src/ProtoRevert.ts
@@ -0,0 +1,95 @@
+import http from 'http';
+import WebSocket, { WebSocketServer } from 'ws';
+import fs from 'fs';
+import { ProtoRevertLink } from './protoRevertLink';
+
+export class ProtoRevert {
+    constructor(private options: {
+        // srcHost: string,
+        srcPort: number, 
+        dstHost: string,
+        dstPort: number }) { }
+    public sessions: ProtoRevertLink[] = [];
+    logs = [] as Array<{ requestUrl: string, response: any }>;
+
+    private async requestListener(req: http.IncomingMessage, res: http.ServerResponse) {
+        const host = this.options.dstHost;
+        const port = this.options.dstPort;
+        const { headers } = req;
+        const path = req.url;
+        http.get({ host, port, path, headers }, (chromeResp) => {
+            const clen = (chromeResp.headers['Content-Length'] || chromeResp.headers['content-length']);
+            console.log(`GET ${path} ${chromeResp.statusCode} Content-Length:${clen}`);
+            res.writeHead(chromeResp.statusCode!, chromeResp.statusMessage, chromeResp.headers);
+            let body = Buffer.alloc(0);
+            chromeResp.on('data', (data) => {
+                body = Buffer.concat([body, data]);
+                res.write(data);
+            })
+            chromeResp.on('end', () => {
+                const txt = JSON.parse(body.toString());
+                this.logs.push({ requestUrl: path || '', response: txt });
+                res.end();
+            });
+            chromeResp.on('close', () => {
+                res.destroy();
+            });
+        });
+    }
+
+    public async start(): Promise<void> {
+        // create http servce + ws server
+        const wss = new WebSocketServer({ noServer: true });
+        const server = http.createServer((req, res) => this.requestListener(req, res));
+
+        wss.on('connection', (ws: WebSocket.WebSocket, request: http.IncomingMessage) => {
+            this.sessions.push(new ProtoRevertLink(ws, request, this.options.dstPort));
+        });
+
+        server.on('upgrade', function upgrade(request, socket, head) {
+            // const { pathname } = parse(request.url);
+            wss.handleUpgrade(request, socket, head, function done(ws) {
+                wss.emit('connection', ws, request);
+            });
+        });
+        // const hostname = this.options.srcHost;
+        await new Promise<void>(resolve => server.listen(this.options.srcPort, resolve));
+    }
+
+    /**
+     * gen corresponding code
+     */
+    public writeSessions(prefix: string) {
+        for (let i = 0; i < this.sessions.length; i++) {
+            const session: ProtoRevertLink = this.sessions[i];
+            let code = "";
+            code += 'import Devtools from "@u4/chrome-remote-interface";\r\n';
+            if (session.rawData.length)
+                code += 'import fs from "fs";\r\n';
+
+            code += "\r\n";
+
+            if (session.rawData.length) {
+                code += 'function getContent(id: number): string {\r\n';
+                code += `  return fs.readFileSync("${prefix}_data_" + id + ".js", {encoding: "utf-8"});\r\n`;
+                code += '}\r\n';
+            }
+            for (let id = 0; id < session.rawData.length; id++) {
+                fs.writeFileSync(prefix + "_data_" + (id + 1) + ".js", session.rawData[id], { encoding: 'utf8' });
+            }
+            // const session = protoRev.sessions[protoRev.sessions.length - 1];
+            code += `async function run${i + 1}(devtools: Devtools) {\r\n`;
+            if (session.endpoint.includes('devtools/browser'))
+                code += '  const cdp = await devtools.connectFirst("browser");';
+            else
+                code += '  const cdp = await devtools.connectFirst("page");';
+            code += '\r\n';
+            code += session.writeSession();
+            code += '}\r\n';
+            code += '\r\n';
+            code += `run${i + 1}(new Devtools());\r\n`;
+
+            fs.writeFileSync(`${prefix}${i + 1}.ts`, code, { encoding: 'utf8' });
+        }
+    }
+}
---
+++ proxy/src/cli.ts
@@ -0,0 +1,35 @@
+import { program } from "commander";
+import { ProtoRevert } from "./ProtoRevert";
+import * as readline from 'readline';
+import { stdin as input, stdout as output } from 'node:process';
+
+program
+    .option("-h, --dest-host <ip>", "host to to connect to", "127.0.0.1")
+    // .option("--listen <ip>", "ip to listen for incomming connection", "127.0.0.1")
+    .option("-s, --source <port>", "address to listen for incomming connextion", "9223")
+    .option("-d, --destination <port>", "Address of the Chrome developer port", "9222")
+    .option("-p, --prefix <name>", "output fule prefix", "code");
+program.parse();
+
+const {source, destination, destHost, prefix } = program.opts();
+// const limit = options.first ? 1 : undefined;
+// console.log(program.args[0].split(options.separator, limit));
+
+const protoRevert = new ProtoRevert({
+    srcPort: Number(source),
+    dstPort: Number(destination),
+    dstHost: destHost,
+    // srcHost: options.listen,
+});
+
+async function main() {
+    await protoRevert.start();
+    const rl = readline.createInterface({ input, output });
+    rl.question("Press any Enter to terminate the session", () => {
+        console.log(`Generating script... with ${prefix} prefix`);
+        rl.close();
+        protoRevert.writeSessions(prefix);
+        console.log('all done');
+        process.exit(0);
+    });
+}
