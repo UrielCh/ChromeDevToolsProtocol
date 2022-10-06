fcb68d20e68eca12e83a42b7ed630b39b702d924
UrielCh <uriel.chemouni@gmail.com>
2022-10-06 14:43:42 +0300

    update build script

--- chrome-remote-interface/_build.ts
+++ chrome-remote-interface/_build.ts
@@ -1,6 +1,8 @@
 import { build, emptyDir } from "https://deno.land/x/dnt@0.30.0/mod.ts";
 // check version here: https://www.npmjs.com/package/@u4/chrome-remote-interface
 // deno run -A _build.ts 0.4.5
+// cd npm
+// npm publish
 if (!Deno.args[0]) {
   console.error('Missing version number')
   Deno.exit(-1);
