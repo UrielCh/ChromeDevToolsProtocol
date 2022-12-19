f0c29681be8c5e3249b1eb223069a178faa77b41
UrielCh <uriel.chemouni@gmail.com>
2022-12-19 09:30:40 +0200

    fix dnt issue

--- chrome-remote-interface/deps.ts
+++ chrome-remote-interface/deps.ts
@@ -1,2 +1,9 @@
 export { EventEmitter } from "https://deno.land/x/event@2.0.0/mod.ts";
-export { type ErrnoException } from "https://deno.land/std@0.126.0/node/internal/errors.ts";
+// export { type ErrnoException } from "https://deno.land/std@0.126.0/node/internal/errors.ts";
+
+export interface ErrnoException extends Error {
+    errno?: number;
+    code?: string;
+    path?: string;
+    syscall?: string;
+}
